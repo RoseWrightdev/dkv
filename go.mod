abac015c5eed7468909178a961e21c75a3979827
Rose Wright <rosesophiewright@gmail.com>
2026-05-17 00:42:02 -0500

    feat: replace custom FNV-1a hashing with xxhash

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 
 require (
 	github.com/armon/go-metrics v0.4.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/btree v1.1.3 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
--- utils.go
+++ utils.go
@@ -1,29 +1,13 @@
 package dkv
 
-// hashFunc implements the FNV-1a hash algorithm.
+import "github.com/cespare/xxhash/v2"
+
+// hashFunc implements the extremely fast xxhash algorithm for strings.
 func hashFunc(key string) hashKey {
-	const (
-		offset64 = 14695981039346656037
-		prime64  = 1099511628211
-	)
-	var hash hashKey = offset64
-	for i := 0; i < len(key); i++ {
-		hash ^= hashKey(key[i])
-		hash *= prime64
-	}
-	return hash
+	return xxhash.Sum64String(key)
 }
 
-// hashBytes implements the FNV-1a hash algorithm for byte arrays.
+// hashBytes implements the extremely fast xxhash algorithm for byte arrays.
 func hashBytes(data []byte) uint64 {
-	const (
-		offset64 = 14695981039346656037
-		prime64  = 1099511628211
-	)
-	var hash uint64 = offset64
-	for i := range data {
-		hash ^= uint64(data[i])
-		hash *= prime64
-	}
-	return hash
+	return xxhash.Sum64(data)
 }
