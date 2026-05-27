62ec6952095c97aedebc244fe2c90e49491ca85a
Rose Wright <rosesophiewright@gmail.com>
2026-05-27 09:40:33 -0500

    perf(cluster): implement dynamic safe port allocation and resource-exhaustion skipping for cluster benchmarks

--- cluster_bench_test.go
+++ cluster_bench_test.go
@@ -13,7 +13,8 @@ func BenchmarkNewCluster(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				cluster, err := NewCluster(nodes, "", insecure.NewCredentials())
 				if err != nil {
-					b.Fatal(err)
+					b.Skipf("Skipping size %d due to resource/socket/port limits: %v", nodes, err)
+					return
 				}
 				cluster.Stop()
 			}
